4d0bcb53c370a89760d7c6e376c6df4ebf96e9dc
Sanjeev <bhusalsanjeev23@gmail.com>
2022-07-08 22:47:31 +0545

    Created a function get_breach_count
    The function takes two parameters, hash_to_check and hashes_list. The
    first parameter is the hashed version of user's password and second is
    the list of all the hashed passwords returned by api.
    
    Function checks if user's hashed password has been in any data breach.
    If yes, it returns the number of times it has been in data breach.

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import hashlib
 class InvalidQueryException(Exception):
     pass
 
+def get_breach_count(hash_to_check, hashes_list):
+    hashes_list = (hash.split(":") for hash in hashes_list)
+    breached_count = 0
+    for current_hash, count in hashes_list:
+        if current_hash == hash_to_check:
+            breached_count = count
+            break 
+    return breached_count
+
+
 def call_api(query_char):
     url = f"https://api.pwnedpasswords.com/range/{query_char}"
     r = requests.get(url)
